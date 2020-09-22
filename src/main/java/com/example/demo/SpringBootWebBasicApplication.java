91d14e219c422ec70b9788366dd9f42c6b536748
John Cole <stuartjohncole@gmail.com>
2020-09-22 17:39:33 +0000

    First Commit

---
+++ src/main/java/com/example/demo/SpringBootWebBasicApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebBasicApplication.class, args);
+	}
+
+}
